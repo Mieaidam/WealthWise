58b1b0858dc0ec475ad37c881fc82db1cc5bcf7d
Idam <idam.elazhar@gmail.com>
2025-05-24 15:46:12 +0700

    Updated auth, progress, and added register page

--- auth.js
+++ auth.js
@@ -1,12 +1,34 @@
 // auth.js
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.0/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.8.0/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/11.8.0/firebase-database.js";
 
-const auth = window.auth;
+const firebaseConfig = {
+  apiKey: "AIzaSyAmm1WHSmprAWkf5KiWA6EH6EzRUY3M60A",
+  authDomain: "wealth-wise-cdff0.firebaseapp.com",
+  projectId: "wealth-wise-cdff0",
+  storageBucket: "wealth-wise-cdff0.appspot.com",
+  messagingSenderId: "236778243947",
+  appId: "1:236778243947:web:1c815a671c487509ecae2e",
+  databaseURL: "https://wealth-wise-cdff0-default-rtdb.firebaseio.com"
+};
 
-export function register(email, password) {
-  return createUserWithEmailAndPassword(auth, email, password);
-}
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+const provider = new GoogleAuthProvider();
 
-export function login(email, password) {
-  return signInWithEmailAndPassword(auth, email, password);
+export async function googleSignIn() {
+  const result = await signInWithPopup(auth, provider);
+  const user = result.user;
+  localStorage.setItem('username', user.displayName || user.email);
+
+  // Save progress to Realtime Database
+  const uid = user.uid;
+  await set(ref(database, 'users/' + uid), {
+    username: user.displayName || user.email,
+    levelCompleted: 'Pinjaman'
+  });
+
+  localStorage.setItem('levelCompleted', 'Pinjaman');
 }
